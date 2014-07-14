faada4ccf5e1874efe503edffa94a81577fef0f3
vicanso <vicansocanbico@gmail.com>
2014-07-14 15:40:07 +0800

    commit

---
+++ dest/demo.js
@@ -0,0 +1,51 @@
+(function() {
+  var JTStatsClient, client, dgram, jtSys, os, program, server;
+
+  JTStatsClient = require('jtstats_client');
+
+  os = require('os');
+
+  program = require('commander');
+
+  jtSys = require('./index');
+
+  dgram = require('dgram');
+
+  server = dgram.createSocket('udp4');
+
+  server.bind('9300');
+
+  server.on('message', function(buf) {
+    return console.dir(buf.toString());
+  });
+
+  (function() {
+    return program.version('0.0.1').option('-p, --port <n>', 'stats server port', parseInt).parse(process.argv);
+  })();
+
+  client = new JTStatsClient({
+    port: program.port || '9300',
+    category: "sys-" + (os.hostname())
+  });
+
+  jtSys.setLogClient(client);
+
+  jtSys.filter('network', function(name) {
+    if (name === 'em2') {
+      return false;
+    } else {
+      return true;
+    }
+  });
+
+  jtSys.filter('disk', function(mount) {
+    if (mount === '/dev') {
+      return false;
+    } else {
+      return true;
+    }
+  });
+
+  jtSys.start(10 * 1000);
+
+}).call(this);
---
+++ src/demo.coffee
@@ -0,0 +1,40 @@
+JTStatsClient = require 'jtstats_client'
+os = require 'os'
+program = require 'commander'
+jtSys = require './index'
+dgram = require 'dgram'
+server = dgram.createSocket 'udp4'
+server.bind '9300'
+
+server.on 'message', (buf) ->
+  console.dir buf.toString()
+
+do ->
+  program.version('0.0.1')
+  .option('-p, --port <n>', 'stats server port', parseInt)
+  .parse process.argv
+
+# cpuCheckInterval = 100
+client = new JTStatsClient {
+  port : program.port || '9300'
+  category : "sys-#{os.hostname()}"
+}
+
+
+
+jtSys.setLogClient client
+jtSys.filter 'network', (name) ->
+  if name == 'em2'
+    false
+  else
+    true
+
+jtSys.filter 'disk', (mount) ->
+  if mount == '/dev'
+    false
+  else
+    true
+jtSys.start 10 * 1000
+
+
+
